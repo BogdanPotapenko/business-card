d0d343c8213da39b241ece6cab8cc6d6d05432a3
Bogdan Potapenko <21bogdan09@gmail.com>
2021-07-22 14:01:37 +0300

    feat: added section about-me

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div>
     <navbar />
     <section-home />
+    <section-about-me />
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar'
 import SectionHome from '~/components/sections/SectionHome'
+import SectionAboutMe from '~/components/sections/SectionAboutMe'
 export default {
   components: {
     Navbar,
     SectionHome,
+    SectionAboutMe,
   },
 }
 </script>
