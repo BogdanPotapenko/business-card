905fddb7b9c150ef1d3031dca6d4236e8bc28e9c
Bogdan Potapenko <21bogdan09@gmail.com>
2021-07-29 12:17:36 +0300

    feat: added section skills

---
+++ components/sections/SectionSkills.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="item-3" class="section-skills">
+    <h2 class="section-skills-title">Skills</h2>
+    <p class="section-skills-text">I work in such program as</p>
+    <ul :class="$mq" class="section-skills-list">
+      <li class="section-skills-list-column">
+        <img src="~/assets/Visual_Studio_Code.png" width="90px" height="90px" />
+        <p class="section-skills-img-name">VC Code</p>
+      </li>
+      <li class="section-skills-list-column">
+        <img src="~/assets/GitHub.png" width="90px" height="90px" />
+        <p class="section-skills-img-name">GitHub</p>
+      </li>
+      <li class="section-skills-list-column">
+        <img src="~/assets/html.png" width="90px" height="90px" />
+        <p class="section-skills-img-name">html</p>
+      </li>
+      <li class="section-skills-list-column">
+        <img src="~/assets/css.png" width="90px" height="90px" />
+        <p class="section-skills-img-name">css</p>
+      </li>
+      <li class="section-skills-list-column">
+        <img src="~/assets/JavaScript.png" width="90px" height="90px" />
+        <p class="section-skills-img-name">JavaScript</p>
+      </li>
+      <li class="section-skills-list-column">
+        <img src="~/assets/Vue.png" width="90px" height="90px" />
+        <p class="section-skills-img-name">Vue</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.section-skills {
+  max-width: 1100px;
+  padding: 100px 0;
+  text-align: center;
+  &-title {
+    font-size: 34px;
+    line-height: 1.235;
+    color: $black900;
+  }
+  &-text {
+    padding: 50px 0;
+    font-size: 18px;
+    line-height: 1.222;
+    color: $black900;
+  }
+  &-list {
+    display: flex;
+    list-style-type: none;
+    padding: 0;
+    &.mobile {
+      display: block;
+    }
+    &-column {
+      padding: 30px 0;
+    }
+  }
+  &-img-name {
+    font-size: 14px;
+    line-height: 1.214;
+    color: $grey;
+    padding-top: 10px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <navbar />
     <section-home />
     <section-about-me />
+    <section-skills />
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 import Navbar from '~/components/Navbar'
 import SectionHome from '~/components/sections/SectionHome'
 import SectionAboutMe from '~/components/sections/SectionAboutMe'
+import SectionSkills from '~/components/sections/SectionSkills'
 export default {
   components: {
     Navbar,
     SectionHome,
     SectionAboutMe,
+    SectionSkills,
   },
 }
 </script>
